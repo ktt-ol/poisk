6c58e4db3b5ef73fbbe71e78569280fb1894bd26
Oliver Tonnhofer <olt@bogosoft.com>
2013-07-24 19:59:05 +0200

    add example_config.py

--- example_config.py
+++ example_config.py
@@ -1,9 +1,12 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+# use DEBUG only for local testing
+# DEBUG = True
 
 CSRF_ENABLED = True
-SECRET_KEY = 'lakfdj02r03ijd023ijdo3knsdkjfn'
+
+# use new secret key for each installatino, never commit/publish it
+SECRET_KEY = 'fLuidIU(*ND£LKjnD(DNLKJS'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
